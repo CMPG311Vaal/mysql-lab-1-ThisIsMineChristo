2966965c9e251a3e2f74b4240e5a9e37b3880e46
Christo <27029085@student.g.nwu.ac.za>
2020-02-24 16:08:12 +0200

    Assignment 1

---
+++ Assignment 1_SQL syntax_C Steenekamp_27029085.sql
@@ -0,0 +1,14 @@
+        Question 5.1
+        SELECT * FROM employee WHERE branchName ='Sunnyside';
+
+        Question 5.2
+        SELECT * FROM borrower;
+
+        Question 5.3
+        SELECT accountNumber FROM customerAccount WHERE balance > 900;
+
+        Question 5.4 
+        SELECT accountNumber FROM customerAccount WHERE balance > 600 AND branchName = 'Newtown';
+
+        Question 5.5;
+        SELECT branchName, assets FROM bankBranch;
